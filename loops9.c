1dfac21d2f3622b115500ec84cfcbdab2b75bad7
JoaoMoniz123 <58425770+JoaoMoniz123@users.noreply.github.com>
2020-02-18 10:28:57 -0100

    Add files via upload

---
+++ loops9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (void){
+
+    int num;
+
+    printf("**************\n");
+    printf("Contagem de 1 a 10\n");
+    printf("**************\n");
+
+    for (num = 1; num <= 10; num++){
+        printf(" %d ", num);
+    }
+}
